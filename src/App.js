6de76a5d7133ab20bcfcfdc07d57b58c482a70ad
Liam Morley <liammorley123@live.co.uk>
2021-03-12 14:57:57 +0000

    add dog api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 import { Button, TimePicker } from "blob-components";
 
+const DOG_API = "https://dog.ceo/api/breeds/image/random";
 function App() {
+  const [dogUrl, setDogUrl] = useState("");
+
+  async function FetchTheApi() {
+    let result = await fetch(DOG_API);
+    let data = await result.json();
+    setDogUrl(data.message);
+    //console.log(data.message);
+    //console.log(data);
+  }
+
+  useEffect(() => {
+    FetchTheApi();
+  }, []);
+
   return (
     <div className="App">
-      <Button/>
-      <TimePicker/>
+      <Button />
+      {/* <TimePicker /> */}
+      <img src={dogUrl} alt="cute dog" />
     </div>
   );
 }
