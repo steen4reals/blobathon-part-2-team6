69da6a4fcf3acc3d7869c46cd9dce45bb31024d0
Roxana Tot <roxana.tot@gmail.com>
2021-03-12 15:58:35 +0000

    Displaying more than one dog

--- src/App.js
+++ src/App.js
@@ -2,29 +2,43 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
 
-import { Button, StarRating} from "blob-components";
+import { Button, StarRating, NumberInput} from "blob-components";
 
 const DOG_API = "https://dog.ceo/api/breeds/image/random";
+
 function App() {
-  const [dogUrl, setDogUrl] = useState("");
+  const [dogUrl, setDogUrl] = useState([]);
+  const [input, setInput] = useState(1);
 
-
-  async function FetchTheApi() {
-    let result = await fetch(DOG_API);
+  async function FetchTheApi(input) {
+    let result = await fetch(`${DOG_API}/${input}`);
     let data = await result.json();
+    console.log("sdgfsgs");
+    console.log(input);
     setDogUrl(data.message);
     //console.log(data.message);
     //console.log(data);
   }
 
   useEffect(() => {
-    FetchTheApi();
-  }, []);
+    FetchTheApi(input);
+  }, [input]);
+
+  function handleInput(event){
+    setInput(event.target.value);
+  }
 
   return (
     <div className="App">
       <Button children ={"click for more cuteness"} onClick ={FetchTheApi}/>
-      <img src={dogUrl} alt="cute dog" />
+      <NumberInput max={5} min={1} variant={"small"} onInput={handleInput}/>
+      {dogUrl.map((dog)=>{
+        return(
+          <li>
+           <img src={dog} alt="cute dog"/>
+          </li>
+          )
+      })}
       <h1>Rate the Cuteness</h1>
       <StarRating/>
     </div>
